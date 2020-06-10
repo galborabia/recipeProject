62292c13563d524b42c0f256087d950b447d3e62
arazi_chen <arazic@post.bgu.ac.il>
2020-06-10 17:06:55 +0300

    sql

--- sql scripts/insertInstriction.sql
+++ sql scripts/insertInstriction.sql
@@ -26,6 +26,24 @@ VALUES
 ','Serving dishes','basil'
 ),
 ( -- first row: values for the columns in the list above
+ '3', '1', 'In large bowl, mix first 4 ingredients. Mix water and oil; add to flour mixture.Turn onto floured surface; knead for 2 minutes.Place in a greased bowl; turning to grease top.Cover and let rise for 20 minutes.',
+ 'large bowl, Sensing tool, ','water, oil, flour'
+),
+( -- first row: values for the columns in the list above
+ '3', '2', 'greased pizza pan.
+Pat into a circle.
+Topping: Mix first 5 ingredients and spread over crust.
+Put a few pepperoni slices on top of sauce.',
+ 'nothing','flour, salt, sugar, fast rise yeast, water, oil,pepperoni slices, sauce'
+),
+( -- first row: values for the columns in the list above
+ '3', '3', 'Sprinkle with 1/2 the mozzeralla; 1/2 the monterey jack, and 1/2 the parmesan.
+Put the rest of the pepperoni on.
+Repeat the cheese layer.
+Bake at 400* for 20 minutes or until light brown',
+ 'nothing','mozzeralla,  monterey jack'
+),
+( -- first row: values for the columns in the list above
  '4', '1', 'Toss the chicken thighs in half the spices and a pinch of salt until completely coated. Heat 1 tbsp oil in a large sauté pan with a lid. Fry chicken, skin-side down, for 10 mins until golden brown, turn over, then cook for 2 mins before removing from the pan.',
  'pan, Sensing tool','chicken thighs, oil,onions ,garlic, couscous'
 ),
@@ -48,6 +66,54 @@ VALUES
 ( -- first row: values for the columns in the list above
  '4', '6', 'then leave to stand for 5 mins until the couscous is cooked through. Fluff through half the parsley and the lemon zest, then sit the chicken on top. Scatter with the rest of the parsley and zest before serving.',
  'Sensing tool','lemon juice, parsley ,lemon zest'
+),
+( -- first row: values for the columns in the list above
+ '5', '1', 'Sauté the onions in the oil until translucent. Add the beets, salt and pepper, and sauté a few minutes more.
+Add the tomato paste and 2 cups water and bring to a boil.',
+ 'pan','onions ,beets, salt and pepper, tomato paste, water '
+),
+( 
+ '5', '2', 'Brown the meat in the olive oil. Season with salt, pepper and ras el hanout.',
+ 'nothing','olive oil., salt and pepper'
+),
+( 
+ '5', '3', 'Mix all the ingredients together until it forms a soft. Remove a small piece of dough and roll it into a ball. Flatten it and place a teaspoon of the ground beef mixture in the center. Fold the dough around the beef and drop into the soup.',
+ 'nothing','beef, salt and pepper'
+),
+( 
+ '5', '4', 'Continue until all the dough has been used. Simmer the soup with the dumplings for 2 hours before serving.',
+ 'nothing','nothing'
+),
+( 
+ '6', '1', 'Mix the flour, water, eggs, oil, lemon juice, orange or rose blossom water, and salt and knead until obtaining a smooth and homogeneous dough.',
+ 'large bowl',
+ 'flour,water, eggs, oil, lemon juice, orange or rose blossom water, and salt and knead '
+),
+( 
+ '6', '2', 'Divide the dough into 3 pieces of equal size. Cover each piece with plastic wrap and let them rest for 45 minutes in a cool place (not in the fridge). This stage allows the dough to become less elastic so it will be easier to roll.',
+ 'nothing',
+ 'nothing'
+),
+( -- first row: values for the columns in the list above
+ '6', '3', 'Sprinkle the work surface with cornstarch and, using a rolling pin, roll the first dough to about 1/16 inch (1mm) thickness. Using a serrated cutter, cut strips of dough, about 2 inches wide and 10 inches long (5x25cm). Repeat with 3 pieces of dough.',
+ 'work surface, rolling pin',
+ 'nothing'
+),
+( -- first row: values for the columns in the list above
+ '6', '4', 'Heat a large pot with vegetable oil over medium heat. Using a fork, prick the end of the dough strip while holding the other end with the other hand. Dip the part held by the fork for a few seconds in the oil then roll the dough quickly around the fork.',
+ 'large pot, fork ',
+ 'vegetable oil '
+),
+( -- first row: values for the columns in the list above
+ '6', '5', 'Carefully remove the fork, and flip with a slotted spoon.
+When it is golden brown, take the strip out of the oil and place in a colander.',
+ 'large pot, fork ',
+ 'vegetable oil '
+),
+( -- first row: values for the columns in the list above
+ '6', '6', 'Syrup -In a non-stick saucepan, combine the honey, lemon juice, orange blossom water (or rose water) and bring to a boil. Turn off the heat and soak the oudnin el kadhi (still hot) in the hot syrup.Sprinkle with sesame seeds or ground pistachios.',
+ 'large pot',
+ 'honey, lemon juice, orange blossom water, sesame '
 )
 -- add more rows here
 GO
--- sql scripts/instertIngredient.sql
+++ sql scripts/instertIngredient.sql
@@ -68,6 +68,48 @@ VALUES
 ( -- second row: values for the columns in the list above
  '2','penne pasta or past of choice','16','ounces' 
 ),
+( -- first row: values for the columns in the list above
+ '3','flour','2.5','cups' 
+),
+( -- first row: values for the columns in the list above
+ '3','salt','1.5','teaspoon' 
+),
+( -- first row: values for the columns in the list above
+ '3','sugar','1','teaspoon' 
+),
+( -- first row: values for the columns in the list above
+ '3','fast rise yeast','1','tablespoon' 
+),
+( -- first row: values for the columns in the list above
+ '3','water (120*)','1','cup' 
+),
+( -- first row: values for the columns in the list above
+ '3','oil','1','tablespoon' 
+),
+( -- first row: values for the columns in the list above
+ '3','tomato sauce','0.25','cup' 
+),
+( -- first row: values for the columns in the list above
+ '3','italian seasoning','1','teaspoon' 
+),
+( -- first row: values for the columns in the list above
+ '3','garlic powder','0.5','teaspoon' 
+),
+( -- first row: values for the columns in the list above
+ '3','pepper','0.125','teaspoon' 
+),
+( -- first row: values for the columns in the list above
+ '3','pepperoni slices','1.5','cups' 
+),
+( -- first row: values for the columns in the list above
+ '3','shredded mozzarella cheese','1.5','cup' 
+),
+( -- first row: values for the columns in the list above
+ '3','shredded monterey jack cheese','1','cup' 
+),
+( -- first row: values for the columns in the list above
+ '3','grated parmesan cheese','1','tablespoons' 
+),
 ( -- second row: values for the columns in the list above
  '4','skin on, bone-in chicken thighs','8','complete' 
 ),
@@ -97,6 +139,69 @@ VALUES
 ),
 ( -- second row: values for the columns in the list above
  '4','parsley','1','small bunch flat-leaf' 
+),
+( -- second row: values for the columns in the list above
+ '4','parsley','1','small bunch flat-leaf' 
+),
+( -- first row: values for the columns in the list above
+ '5','onions','3','comlete' 
+),
+( -- first row: values for the columns in the list above
+ '5','beets','4','comlete' 
+),
+( -- first row: values for the columns in the list above
+ '5','oil','9','tbsp' 
+),
+( -- first row: values for the columns in the list above
+ '5','omato paste','3','tbsp' 
+),
+( -- first row: values for the columns in the list above
+ '5','water','2','cups' 
+),
+( -- first row: values for the columns in the list above
+ '5','Salt','2.5','tbsp' 
+),
+( -- first row: values for the columns in the list above
+ '5','Pepper','2','tbsp' 
+),
+( -- first row: values for the columns in the list above
+ '5','beef','500','grams ground' 
+),
+( -- first row: values for the columns in the list above
+ '5','Morrocan spice blend','0.5','tsp' 
+),
+( -- first row: values for the columns in the list above
+ '5','semolina','2','cups' 
+),
+( -- first row: values for the columns in the list above
+ '6','flour','4','cups' 
+),
+( -- first row: values for the columns in the list above
+ '6','eggs lightly beaten','4','complete' 
+),
+( -- first row: values for the columns in the list above
+ '6','vegetable oil','4','tablespoons' 
+),
+( -- first row: values for the columns in the list above
+ '6','water','3','tablespoons' 
+),
+( -- first row: values for the columns in the list above
+ '6','lemon juice','7','tablespoons' 
+),
+( -- first row: values for the columns in the list above
+ '6','salt','2','pinches' 
+),
+( -- first row: values for the columns in the list above
+ '6','orange blossom water','6','tablespoons' 
+),
+( -- first row: values for the columns in the list above
+ '6','Cornstarch','4','tablespoons' 
+),
+( 
+ '6','honey','8','tablespoons' 
+),
+( 
+ '6','sesame','2','tablespoons' 
 )
 -- add more rows here
 GO
